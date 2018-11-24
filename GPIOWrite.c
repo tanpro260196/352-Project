ad9a6ceddb265c3f44d303c6f1502f0b020a440c
Cao Tan <tanpro260196@gmail.com>
2018-11-24 12:26:41 -0800

    Update GPIOWrite.c

--- GPIOWrite.c
+++ GPIOWrite.c
@@ -20,12 +20,15 @@ volatile uint32_t ui32Loop;
 int main(void)
 {
     //Not sure what these do, but without them the code doesn't work :)
+    //The first one is to enable port D. Another command to do this is SYSCTL_RCGCGPIO_R |= 0x08
     SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOD;
+    //This one is to start the MCU CPU and set it to 40Mhz
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 
     //
     // Enable the GPIO port that is used for the on-board LED.
     //
+    //Well, technically, you don't meed to enable port D again. You already do that above. Haven't try it though.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
     GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
 
